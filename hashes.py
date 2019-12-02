35ce1f2c76ae763c15135ca80a53dd3cebf8fb63
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-12-02 13:45:11 -0500

    Guided Project Day 1

---
+++ hashes.py
@@ -0,0 +1,15 @@
+import time
+import hashlib
+import bcrypt
+
+key = b"hello"
+
+def djb2(key):
+    # Start from arbitrarily large prime
+    hash_value = 5381
+    # bit-shift and sum value for each char
+    for char in key:
+        hash_value = ((hash_value << 5) + hash_value) + char
+    return hash_value
+
+print(djb2(key))
